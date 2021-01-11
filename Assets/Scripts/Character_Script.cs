b6a7e6bb142b1254bdb38939334f529cc7b8a596
simkaiwen <kaiphonegt@gmail.com>
2021-01-11 20:27:29 +0800

    lab 14

---
+++ Assets/Scripts/Character_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Script : MonoBehaviour
+{
+    int speed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Object_Script.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Object_Script : MonoBehaviour
+{
+    public GameObject RedText;
+    public GameObject YellowText;
+    public GameObject OrangeText;
+    public GameObject PurpleText;
+
+    private int yellowCounter;
+    private int redCounter;
+    private int orangeCounter;
+    private int purpleCounter;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.CompareTag("Red"))
+        {
+            if (collision.collider.gameObject.CompareTag("Player"))
+            {
+                redCounter++;
+
+                RedText.GetComponent<Text>().text = "Red: " + redCounter;
+                audioSource.Play();
+            }
+        }
+
+        if (gameObject.CompareTag("Yellow"))
+        {
+            if (collision.collider.gameObject.CompareTag("Player"))
+            {
+                yellowCounter++;
+
+                YellowText.GetComponent<Text>().text = "Yellow: " + yellowCounter;
+                audioSource.Play();
+            }
+        }
+
+        if (gameObject.CompareTag("Orange"))
+        {
+            if (collision.collider.gameObject.CompareTag("Player"))
+            {
+                orangeCounter++;
+
+                OrangeText.GetComponent<Text>().text = "Orange: " + orangeCounter;
+                audioSource.Play();
+            }
+        }
+
+        if (gameObject.CompareTag("Purple"))
+        {
+            if (collision.collider.gameObject.CompareTag("Player"))
+            {
+                purpleCounter++;
+
+                PurpleText.GetComponent<Text>().text = "Purple: " + purpleCounter;
+                audioSource.Play();
+            }
+        }
+    }
+}
